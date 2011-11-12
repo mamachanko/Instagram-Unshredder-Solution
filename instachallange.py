3d1da6468d26ba6a7a66d3f24ccf17f7e130ef51
Max Brauer <max@rootswiseyouths.com>
2011-11-12 20:53:35 +0100

    tweaks and bonuns challenge

--- instachallange.py
+++ instachallange.py
@@ -9,18 +9,40 @@
 
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
+
+def mostCommon(l):
+	"""
+	Find the most common element in a list.
+	"""
+	d = {}
+	for i in l:
+		if i in d:
+			d[i] += 1
+		else:
+			d[i] = 1
+	return sorted(d, key = d.get, reverse = True)[0]
 
 def columnWidth(shred):
 	"""
-	Returns the column width of each piece.
+	Returns the column width of each column given
+	a shredded image.
 	"""
 	# this where the bonus challenge could be solved
 	# but until then simply return fixed 32
-	return 32
+	# return 32
+	#
+	# BONUS CHALLENGE
+	x = map(np.sum, reduce(lambda x,y: x+y, shred.astype(float)))
+	x = np.abs(np.diff(x))
+	x /= np.max(np.abs(x),axis=0)
+	x[x<.2]=0
+	x = np.diff(np.nonzero(x)[0])
+	return mostCommon(x)
 
 def findMatch(column, othercolumns, left):
 	"""
-	Find the best left or right side match for a given column and a list of
+	Find the best left or right side match for a given column from a list of
 	other columns. If param 'left' is True look for a left side match other
 	wise right side.
 	"""
@@ -55,30 +77,60 @@ def bestLMatch(column, othercolumns):
 	return findMatch(column, othercolumns, left=True)	
 
 def bestMatch(column, othercolumns):
-	
+	"""
+	Returns the best match for a column from a given list of
+	columns. If neither right nor left side match is better,
+	return the right side match.
+	"""
+	# get the best left side match and it's euclidean distance
 	ldist, lmatch = bestLMatch(column, othercolumns)
+	# same for the right hand side
 	rdist, rmatch = bestRMatch(column, othercolumns)
+	# return the better/ the one with the smaller
+	# euclidean distance
 	if ldist < rdist:
 		return lmatch, 'l'
 	return rmatch, 'r'
 
 def match(column, othercolumns):
+	"""
+	Match a column to another given a list of other columns
+	by glueing them together accordingly. Returns a list
+	of the new piece and the rest of columns.
+	"""
+	# get the best match for the column
 	matchidx, side = bestMatch(column, othercolumns)
-	print 'matching with %s on %s' % (matchidx, side) 
+	# pop that best match from the othercolumns list
 	matchcolumn = othercolumns.pop(matchidx)
+	# depending on the side merge those two columns
 	if side == 'l':
 		glued = np.hstack((matchcolumn, column))
 	elif side == 'r':
 		glued = np.hstack((column, matchcolumn))
+	# return a list containing all remaining pieces)
 	return othercolumns, glued
 
-shred = np.asarray(Image.open('./TokyoPanoramaShredded.png'))
-columns = np.split(shred, 20, axis=1)
+if __name__ == '__main__':
+	# read the image
+	# shred = np.asarray(Image.open('./TokyoPanoramaShredded.png'))
+	shred = np.asarray(Image.open('./sample_shredded.png'))
+	# determine the number of columns
+	cwidth = columnWidth(shred)
+	print 'estimated column width %s' % cwidth
+	nocolumns = shred.shape[1]/cwidth
+	# get the columns
+	columns = np.split(shred, nocolumns, axis=1)
+	# match piece by piece until only on whole remains
+	while len(columns) > 1:
+		column = columns.pop(0)
+		othercolumns, glued = match(column, columns)
+		othercolumns.append(glued)
+		columns = othercolumns
+	# save result
+	Image.fromarray(columns[0]).save('./result.png')
 
-while len(columns) > 1:
-	column = columns.pop(0)
-	othercolumns, glued = match(column, columns)
-	othercolumns.append(glued)
-	columns = othercolumns
-
-Image.fromarray(columns[0]).save('./result.png')
+	#x = map(np.sum, reduce(lambda x,y: x+y, shred.astype(float)))
+	#x = np.abs(np.diff(x))
+	#x /= np.max(np.abs(x),axis=0)
+	#x[x<.2]=0
+	#plt.scatter(x])
